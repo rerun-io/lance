2ec1b9cb71a8d0da30d2bd9c96a5a37048af8543
Andrea Reale <andrea@rerun.io>
2025-09-25 12:01:34 +0200

    support https urls for Azure Blob Storage
    `object_store` supports several ways of encoding the address of Azure
    Blob Storage URLs, as specified [here](https://github.com/apache/arrow-rs-object-store/blob/b82979d44a916cf1615e719c8e80800766fd6efe/src/azure/builder.rs#L531).
    
    Before this change, the lance wrapper only supported the `az` scheme.
    However, the other schemes are significant too because they allow to
    encode extra information (e.g., the Azure Storage account name) or
    access different store type (e.g., Files Blob Store).
    
    This change, maps `https` object URIs to Azure, so that the underlying
    `object_store` support can be effectively used.
    
    Signed-off-by: Andrea Reale <andrea@rerun.io>

--- rust/lance-io/src/object_store/providers/azure.rs
+++ rust/lance-io/src/object_store/providers/azure.rs
@@ -10,6 +10,7 @@ use snafu::location;
 
 use object_store::{
     azure::{AzureConfigKey, MicrosoftAzureBuilder},
+    path::Path,
     RetryConfig,
 };
 use url::Url;
@@ -87,6 +88,20 @@ impl AzureBlobStoreProvider {
 
 #[async_trait::async_trait]
 impl ObjectStoreProvider for AzureBlobStoreProvider {
+    fn extract_path(&self, url: &Url) -> Result<Path> {
+        // Azure https paths in ObjectSore encode the container name as the first path segment.
+        // The actual object path starts from the second segment.
+        if url.scheme() == "https" {
+            url.path_segments()
+                .map(|s| Path::from_iter(s.skip(1)))
+                .ok_or_else(|| {
+                    Error::invalid_input(format!("Invalid Azure URL: {url}"), location!())
+                })
+        } else {
+            Ok(Path::from(url.path()))
+        }
+    }
+
     async fn new_store(&self, base_path: Url, params: &ObjectStoreParams) -> Result<ObjectStore> {
         let block_size = params.block_size.unwrap_or(DEFAULT_CLOUD_BLOCK_SIZE);
         let mut storage_options =
@@ -160,6 +175,16 @@ mod tests {
 
         let url = Url::parse("az://bucket/path/to/file").unwrap();
         let path = provider.extract_path(&url).unwrap();
+        let expected_path = object_store::path::Path::from("path/to/file");
+        assert_eq!(path, expected_path);
+    }
+
+    #[test]
+    fn test_azure_store_https_path() {
+        let provider = AzureBlobStoreProvider;
+
+        let url = Url::parse("https://account.blob.core.windows.net/bucket/path/to/file").unwrap();
+        let path = provider.extract_path(&url).expect("Failed to extract path");
         let expected_path = object_store::path::Path::from("path/to/file");
         assert_eq!(path, expected_path);
     }
